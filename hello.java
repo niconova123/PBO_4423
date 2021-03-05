2a5e4178c661511db684ff0bf9792c0fbc3cedec
nico <79845116+niconova123@users.noreply.github.com>
2021-03-06 00:36:32 +0700

    tugas prak1

---
+++ hello.java
@@ -0,0 +1,27 @@
+//nama:Nico Syahputra DN/A11.2019.12292
+public class hello {  
+    public static void main(String[] args){
+        //a
+        int bil = -5, bil2 = 8, bil3 = 6;
+        int hasil = bil + bil2  * bil3;
+        System.out.println(hasil);
+        //b
+        int angka1 = 55, angka2 = 9, angka3 = 9;
+        int hasilnya = (angka1 + angka2) % angka3;
+        System.out.println(hasilnya);
+        //c
+        int bilangan = 20, bilangan1 = -3, bilangan2 = 5, bilangan3 = 8;
+        int hasill = bilangan + bilangan1 * bilangan2 / bilangan3;
+        System.out.println(hasill);
+        //d
+        int number = 5, number1 = 15, number2 = 3,number3=2,number4=8,number5=3;
+        int total = number + number1 / number2 * number3 - number4 % number5;
+        System.out.println(total);
+
+        
+
+
+
+    }
+    
+}
